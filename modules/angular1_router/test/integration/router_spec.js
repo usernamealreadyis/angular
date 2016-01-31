a28cc996b352ac41d558f992d75e74349ce772b8
Brandon Roberts <robertsbt@gmail.com>
2016-01-31 14:52:19 -0600

    fix(router): Added route data to normalized async route

--- modules/angular1_router/test/integration/router_spec.js
+++ modules/angular1_router/test/integration/router_spec.js
@@ -44,10 +44,33 @@ describe('router', function () {
     expect(elt.text()).toBe('Home');
   }));
 
+  it('should work when an async route is provided route data', inject(function($location, $q) {
+    registerComponent('homeCmp', {
+      template: 'Home ({{vm.isAdmin}})',
+      controllerAs: 'vm',
+      $routerOnActivate: function(next, prev) {
+        this.isAdmin = next.routeData.data.isAdmin;
+      }
+    });
+
+    registerComponent('app', {
+      template: '<div ng-outlet></div>',
+      $routeConfig: [
+        { path: '/', loader: function() { return $q.when('homeCmp'); }, data: { isAdmin: true } }
+      ]
+    });
+
+    compile('<app></app>');
+
+    $location.path('/');
+    $rootScope.$digest();
+    expect(elt.text()).toBe('Home (true)');
+  }));
+
   function registerComponent(name, options) {
     var controller = options.controller || function () {};
 
-    ['$onActivate', '$onDeactivate', '$onReuse', '$canReuse', '$canDeactivate'].forEach(function (hookName) {
+    ['$routerOnActivate', '$routerOnDeactivate', '$routerOnReuse', '$routerCanReuse', '$routerCanDeactivate'].forEach(function (hookName) {
       if (options[hookName]) {
         controller.prototype[hookName] = options[hookName];
       }
